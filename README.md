299ff7ed2d0737ed80a64aa2f5e9887955d03f2f
qipeili <ashqi@hotmail.com>
2020-05-05 15:39:30 +0800

    Initial release 第一次打包

---
+++ README.md
@@ -0,0 +1,43 @@
+# c-c---run-anywhere README
+
+全称 C/C++ Code Run Anywhere
+
+vccode打开c/cpp文件，立即编译并运行。 需要安装gcc。
+Ctrl + Shift + P 打开命令栏输入： 
+
+C&R C/C++ 编译并运行文件
+Compile C/C++ -S 编译成汇编代码
+
+与我联系：ashqi@hotmail.com
+
+## Requirements
+
+需要安装gcc
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+* `myExtension.enable`: enable/disable this extension
+* `myExtension.thing`: set to `blah` to do something
+
+## Known Issues
+
+Calling out known issues can help limit users opening duplicate issues against your extension.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 1.0.0
+
+Initial release 第一次打包
+
+-----------------------------------------------------------------------------------------------------------
+
+### For more information
+
+* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
+* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+
+**Enjoy!**
---
+++ extension.js
@@ -0,0 +1,95 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	function paramStr(param){
+		const headStr = "gcc ";
+
+		const srcName = vscode.window.activeTextEditor.document.fileName;
+		let terminal = vscode.window.activeTerminal;
+		terminal.sendText('\n');
+
+		let tragName = (srcName.split("."))[0];
+
+		let index = srcName.lastIndexOf("\\");
+		let path = srcName.substr(0, index + 1);
+		terminal.sendText("cd " + path);
+
+		let commandStr = "";
+		
+
+		switch(param){
+			case 0:
+				commandStr = headStr + "-o " + tragName + " " + srcName;
+				break;
+			case 1:
+				commandStr = headStr + "-s " + srcName;
+				break;
+			case 2:
+				commandStr = headStr + "-S " + srcName;
+				break;
+		}
+		terminal.sendText(commandStr);
+
+		switch(param){
+			case 0:
+				terminal.sendText(tragName + '.exe');
+				break;
+			case 1:
+				break;
+			case 2:
+				break;
+		}
+
+	}
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "c-c---run-anywhere" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('c-c---run-anywhere.helloWorld', function () {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		const helloWorldStr = "Welcome use c/c++ run anywhere!"
+		vscode.window.showInformationMessage(helloWorldStr);
+
+	});
+
+	let runAnyWhere0 = vscode.commands.registerCommand('c-c---run-anywhere.Run', function () {
+		paramStr(0);
+	});
+
+	let runAnyWhere1 = vscode.commands.registerCommand('c-c---run-anywhere.Run_s', function () {
+		paramStr(1);
+	});
+
+	let runAnyWhere2 = vscode.commands.registerCommand('c-c---run-anywhere.Run_CS', function () {
+		paramStr(2);
+	});
+
+	context.subscriptions.push(disposable);
+
+	context.subscriptions.push(runAnyWhere0);
+	context.subscriptions.push(runAnyWhere1);
+	context.subscriptions.push(runAnyWhere2);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
